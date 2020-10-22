6f03419a671442408b8b582fd2a9c6b0e93366ac
Sekretov Maksim <maximsecret10@gmail.com>
2020-10-22 12:22:12 +0600

    Updates

---
+++ precache-manifest.09e45e17f6367721d5827a983c712813.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7748ef16ea2c5486cca15d5b55f61bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76555002c6d473867cc0",
+    "url": "/static/css/main.5ef78208.chunk.css"
+  },
+  {
+    "revision": "a75f55c10d62ab7894f6",
+    "url": "/static/js/2.e0ac51b7.chunk.js"
+  },
+  {
+    "revision": "2e2193cf939fcf2814e2a63112d5ddfd",
+    "url": "/static/js/2.e0ac51b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76555002c6d473867cc0",
+    "url": "/static/js/main.3b8752e0.chunk.js"
+  },
+  {
+    "revision": "debe4807421d7f7db6c8",
+    "url": "/static/js/runtime-main.725c103c.js"
+  },
+  {
+    "revision": "6d0f79736f6163949d5e2321649480f1",
+    "url": "/static/media/logo-main.6d0f7973.png"
+  },
+  {
+    "revision": "aafbda5a5ea9362570935ac335cdca8d",
+    "url": "/static/media/main-img.aafbda5a.png"
+  }
+]);
